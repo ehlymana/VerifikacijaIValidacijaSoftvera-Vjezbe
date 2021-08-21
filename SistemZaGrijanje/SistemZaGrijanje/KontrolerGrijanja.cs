ca17277ebd8e9004c892090ad35569ced80166c3
Ehlimana <ekrupalija1@etf.unsa.ba>
2021-08-21 18:30:02 +0200

    .NET 5, primjer sa kontrolom grijanja

---
+++ SistemZaGrijanje/SistemZaGrijanje/KontrolerGrijanja.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemZaGrijanje
+{
+    public class KontrolerGrijanja
+    {
+        public static string IndikatorVode { get; set; }
+        public bool Aktivna { get; set; }
+        public double JačinaGrijanja { get; set; }
+
+        public void AutomatskaKontrola(ITermostat termostat, bool aktiviraj)
+        {
+            if (termostat == null)
+                throw new Exception("Nije moguće uspostaviti konekciju!");
+
+            if (!aktiviraj)
+            {
+                Aktivna = false;
+                return;
+            }
+
+            Aktivna = true;
+            if (termostat.dajTemperaturu() < 20)
+                JačinaGrijanja = 1;
+            else if (termostat.dajTemperaturu() > 25)
+                JačinaGrijanja = 0;
+            else
+                JačinaGrijanja = 0.5;
+
+            bool defrost = true;
+            for (int i = 0; i < 10; i++)
+                if (termostat.dajTemperaturu() > 7)
+                    defrost = false;
+            if (defrost)
+                JačinaGrijanja = 0.1;
+        }
+
+        public void Usrednjavanje(ITermostat t)
+        {
+            double srednja = t.Temperature.Average();
+            JačinaGrijanja = Math.Abs(30 - srednja) / srednja;
+        }
+
+        public static string DefrostIndikator()
+        {
+            if (IndikatorVode == "OK")
+                return "KOD-01";
+            else
+                return "KOD-02-ERROR";
+        }
+    }
+}
---
+++ SistemZaGrijanje/SistemZaGrijanje/UnitTest1.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemZaGrijanje
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Test deaktivacije grijanja koristeći dummy
+        /// </summary>
+        [TestMethod]
+        public void TestDeaktivacijeGrijanja()
+        {
+            KontrolerGrijanja k = new KontrolerGrijanja();
+            ITermostat dummy = new Dummy();
+
+            k.Aktivna = true;
+            k.AutomatskaKontrola(dummy, false);
+            Assert.IsFalse(k.Aktivna);
+        }
+
+        /// <summary>
+        /// Test kontrole temperature koristeći tri stuba
+        /// </summary>
+        [TestMethod]
+        public void TestKontroleGrijanja1()
+        {
+            KontrolerGrijanja k = new KontrolerGrijanja();
+
+            ITermostat stub1 = new StubNiskaTemperatura();
+            k.AutomatskaKontrola(stub1, true);
+            Assert.AreEqual(k.JačinaGrijanja, 1);
+
+            ITermostat stub2 = new StubSrednjaTemperatura();
+            k.AutomatskaKontrola(stub2, true);
+            Assert.AreEqual(k.JačinaGrijanja, 0.5);
+
+            ITermostat stub3 = new StubVisokaTemperatura();
+            k.AutomatskaKontrola(stub3, true);
+            Assert.AreEqual(k.JačinaGrijanja, 0);
+        }
+
+        /// <summary>
+        /// Test kontrole temperature koristeći spy
+        /// </summary>
+        [TestMethod]
+        public void TestKontroleGrijanja2()
+        {
+            KontrolerGrijanja k = new KontrolerGrijanja();
+            ITermostat spy = new Spy();
+
+            ((Spy)spy).Opcija = 0;
+            k.AutomatskaKontrola(spy, true);
+            Assert.AreEqual(k.JačinaGrijanja, 1);
+
+            ((Spy)spy).Opcija = 1;
+            k.AutomatskaKontrola(spy, true);
+            Assert.AreEqual(k.JačinaGrijanja, 0.5);
+
+            ((Spy)spy).Opcija = 2;
+            k.AutomatskaKontrola(spy, true);
+            Assert.AreEqual(k.JačinaGrijanja, 0);
+        }
+
+        /// <summary>
+        /// Test usrednjavanja jačine grijanja koristeći fake
+        /// </summary>
+        [TestMethod]
+        public void TestUsrednjavanjaJačineGrijanja()
+        {
+            KontrolerGrijanja k = new KontrolerGrijanja();
+            ITermostat fake = new Fake();
+
+            fake.Temperature = new List<double>()
+            {
+                20, 21, 22, 23, 24
+            };
+            k.Usrednjavanje(fake);
+            double očekivaniRezultat = Math.Abs(30 - fake.Temperature.Average()) / fake.Temperature.Average();
+            Assert.IsTrue(Math.Abs(k.JačinaGrijanja - očekivaniRezultat) < 0.01);
+
+            očekivaniRezultat = ((Fake)fake).DodajTestneTemperature();
+            k.Usrednjavanje(fake);
+            Assert.IsTrue(Math.Abs(k.JačinaGrijanja - očekivaniRezultat) < 0.01);
+        }
+
+        /// <summary>
+        /// Test defrost tehnologije (kada radi) koristeći mock
+        /// </summary>
+        [TestMethod]
+        public void TestDefrostTehnologijeOK()
+        {
+            KontrolerGrijanja k = new KontrolerGrijanja();
+            ITermostat mock = new Mock();
+
+            KontrolerGrijanja.IndikatorVode = "OK";
+            k.AutomatskaKontrola(mock, true);
+            Assert.AreEqual(k.JačinaGrijanja, 0.1);
+        }
+
+        /// <summary>
+        /// Test defrost tehnologije (neočekivano ponašanje) koristeći mock
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestDefrostTehnologijeNeočekivanoPonašanje()
+        {
+            KontrolerGrijanja k = new KontrolerGrijanja();
+            ITermostat mock = new Mock();
+            k.AutomatskaKontrola(mock, true);
+        }
+    }
+}
