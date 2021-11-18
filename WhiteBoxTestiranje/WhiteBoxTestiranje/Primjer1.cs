47c5d58d70c5b30a80cd1d25ec0288eb3d3f96df
Ehlimana <ekrupalija1@etf.unsa.ba>
2021-11-18 09:55:17 +0100

    Primjeri za metrike, white box i code tuning

---
+++ WhiteBoxTestiranje/WhiteBoxTestiranje/Primjer1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetrikeRefaktoringCodeTuning
+{
+    public class Uposlenik
+    {
+        string ime;
+
+        public string Ime { get => ime; set => ime = value; }
+    }
+
+    public class Primjer1
+    {
+        List<Uposlenik> uposlenici = new List<Uposlenik>();
+
+        public List<Uposlenik> PretragaUposlenikaPoNazivu(string naziv)
+        {
+            List<Uposlenik> lista = new List<Uposlenik>();
+            foreach (Uposlenik u in uposlenici)
+            {
+                Uposlenik privremeni = null;
+                if (u.Ime.Contains(naziv))
+                    if (!lista.Contains(u))
+                        privremeni = u;
+
+                if (privremeni != null)
+                    lista.Add(privremeni);
+            }
+
+            if (lista.Count == 0)
+                throw new Exception("Trazeni uposlenik ne postoji!");
+
+            return lista;
+        }
+
+        public List<Uposlenik> PretragaUposlenikaPoNazivuRefaktoring1(string naziv)
+        {
+            List<Uposlenik> lista = new List<Uposlenik>();
+            foreach (Uposlenik u in uposlenici)
+            {
+                Uposlenik privremeni = null;
+                if (u.Ime.Contains(naziv))
+                    privremeni = u;
+
+                if (privremeni != null)
+                    lista.Add(privremeni);
+            }
+
+            if (lista.Count == 0)
+                throw new Exception("Trazeni uposlenik ne postoji!");
+
+            return lista;
+        }
+
+        public List<Uposlenik> PretragaUposlenikaPoNazivuRefaktoring2(string naziv)
+        {
+            List<Uposlenik> lista = new List<Uposlenik>();
+            foreach (Uposlenik u in uposlenici)
+            {
+                if (u.Ime.Contains(naziv))
+                    lista.Add(u);
+            }
+
+            if (lista.Count == 0)
+                throw new Exception("Trazeni uposlenik ne postoji!");
+
+            return lista;
+        }
+
+        public List<Uposlenik> PretragaUposlenikaPoNazivuRefaktoring3(string naziv)
+        {
+            List<Uposlenik> lista = uposlenici.FindAll(u => u.Ime.Contains(naziv));
+
+            if (lista.Count == 0)
+                throw new Exception("Trazeni uposlenik ne postoji!");
+
+            return lista;
+        }
+    }
+}
---
+++ WhiteBoxTestiranje/WhiteBoxTestiranje/Primjer3.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetrikeRefaktoringCodeTuning
+{
+    public class Zaposlenik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodenja { get; set; }
+        public double Plata { get; set; }
+
+        public Zaposlenik(string ime, string prezime, DateTime rodenje, double plata)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodenja = rodenje;
+            Plata = plata;
+        }
+    }
+
+    public class Primjer3
+    {
+        List<Zaposlenik> uposlenici = new List<Zaposlenik>();
+
+        public List<Zaposlenik> Uposlenici { get => uposlenici; set => uposlenici = value; }
+
+        public Zaposlenik PretragaUposlenikaPoNazivu(string naziv)
+        {
+            Zaposlenik privremeni = null;
+            for (int i = 0; i < Uposlenici.Count; i++)
+            {
+                if (Uposlenici[i].Plata < (DateTime.Now.Year - Uposlenici[i].DatumRodenja.Year - 18) * 300)
+                    continue;
+
+                if (Uposlenici[i].Ime.Contains(naziv) || Uposlenici[i].Prezime.Contains(naziv)
+                    || Uposlenici[i].DatumRodenja.ToString().Contains(naziv))
+                        privremeni = Uposlenici[i];
+            }
+            if (privremeni == null)
+                throw new Exception("Trazeni uposlenik ne postoji!");
+
+            return privremeni;
+        }
+
+        public Zaposlenik PretragaUposlenikaPoNazivuTuning1(string naziv)
+        {
+            for (int i = 0; i < Uposlenici.Count; i++)
+            {
+                if (Uposlenici[i].Plata < (DateTime.Now.Year - Uposlenici[i].DatumRodenja.Year - 18) * 300)
+                    continue;
+
+                if (Uposlenici[i].Ime.Contains(naziv) || Uposlenici[i].Prezime.Contains(naziv)
+                    || Uposlenici[i].DatumRodenja.ToString().Contains(naziv))
+                    return Uposlenici[i];
+
+            }
+
+            throw new Exception("Trazeni uposlenik ne postoji!");
+        }
+
+        public Zaposlenik PretragaUposlenikaPoNazivuTuning2(string naziv)
+        {
+            for (int i = 0; i < Uposlenici.Count; i++)
+            {
+                int godineOdPunoljetstva = DateTime.Now.Year - Uposlenici[i].DatumRodenja.Year - 18;
+                int gornjaGranicaPlate = godineOdPunoljetstva * 300;
+
+                if (Uposlenici[i].Plata < gornjaGranicaPlate)
+                    continue;
+
+                bool pronadenoIme = Uposlenici[i].Ime.Contains(naziv),
+                    pronadenoPrezime = Uposlenici[i].Prezime.Contains(naziv),
+                    pronadenDatum = Uposlenici[i].DatumRodenja.ToString().Contains(naziv);
+
+                if (pronadenoIme || pronadenoPrezime || pronadenDatum)
+                    return Uposlenici[i];
+            }
+
+            throw new Exception("Trazeni uposlenik ne postoji!");
+        }
+
+        public Zaposlenik PretragaUposlenikaPoNazivuTuning3(string naziv)
+        {
+            for (int i = 0; i < Uposlenici.Count; i += 4)
+            {
+                int plata1 = (DateTime.Now.Year - Uposlenici[i].DatumRodenja.Year - 18) * 300;
+                int plata2 = (DateTime.Now.Year - Uposlenici[i + 1].DatumRodenja.Year - 18) * 300;
+                int plata3 = (DateTime.Now.Year - Uposlenici[i + 2].DatumRodenja.Year - 18) * 300;
+                int plata4 = (DateTime.Now.Year - Uposlenici[i + 3].DatumRodenja.Year - 18) * 300;
+                int indeksOk = -1;
+                if (Uposlenici[i].Plata > plata1)
+                    indeksOk = i;
+                else if (Uposlenici[i + 1].Plata > plata2)
+                    indeksOk = i + 1;
+                else if (Uposlenici[i + 2].Plata > plata3)
+                    indeksOk = i + 2;
+                else if (Uposlenici[i + 3].Plata > plata4)
+                    indeksOk = i + 3;
+
+                if (indeksOk == -1)
+                    continue;
+
+                bool pronadenoIme = Uposlenici[indeksOk].Ime.Contains(naziv),
+                    pronadenoPrezime = Uposlenici[indeksOk].Prezime.Contains(naziv),
+                    pronadenDatum = Uposlenici[indeksOk].DatumRodenja.ToString().Contains(naziv);
+
+                if (pronadenoIme || pronadenoPrezime || pronadenDatum)
+                    return Uposlenici[indeksOk];
+
+            }
+
+            throw new Exception("Trazeni uposlenik ne postoji!");
+        }
+
+
+        public Zaposlenik PretragaUposlenikaPoNazivuTuning4(string naziv)
+        {
+            int godina = DateTime.Now.Year;
+            for (int i = 0; i < Uposlenici.Count; i += 4)
+            {
+                int plata1 = (godina - Uposlenici[i].DatumRodenja.Year - 18); int plata11 = plata1;
+                int plata2 = (godina - Uposlenici[i + 1].DatumRodenja.Year - 18); int plata22 = plata2;
+                int plata3 = (godina - Uposlenici[i + 2].DatumRodenja.Year - 18); int plata33 = plata3;
+                int plata4 = (godina - Uposlenici[i + 3].DatumRodenja.Year - 18); int plata44 = plata4;
+
+                for (int j = 0; j < 300; j++)
+                {
+
+                    plata11 += plata1;
+                    plata22 += plata2;
+                    plata33 += plata3;
+                    plata44 += plata4;
+                }
+
+                int indeksOk = -1;
+                if (Uposlenici[i].Plata > plata11)
+                    indeksOk = i;
+                else if (Uposlenici[i + 1].Plata > plata22)
+                    indeksOk = i + 1;
+                else if (Uposlenici[i + 2].Plata > plata33)
+                    indeksOk = i + 2;
+                else if (Uposlenici[i + 3].Plata > plata44)
+                    indeksOk = i + 3;
+                if (indeksOk == -1)
+                    continue;
+
+                bool pronadenoIme = Uposlenici[indeksOk].Ime.Contains(naziv),
+                    pronadenoPrezime = Uposlenici[indeksOk].Prezime.Contains(naziv),
+                    pronadenDatum = Uposlenici[indeksOk].DatumRodenja.ToString().Contains(naziv);
+
+                if (pronadenoIme || pronadenoPrezime || pronadenDatum)
+                    return Uposlenici[indeksOk];
+
+            }
+
+            throw new Exception("Trazeni uposlenik ne postoji!");
+        }
+    }
+
+    [TestClass]
+    public class Primjer5Test
+    {
+        [TestMethod]
+        public void TestTuning()
+        {
+            Primjer3 klasa = new Primjer3();
+            for (int i = 0; i < 1000000; i++)
+                klasa.Uposlenici.Add(new Zaposlenik("Ime" + i, "Prezime", DateTime.Now.AddMonths(-18 * 12), 20 * 300));
+
+            // prvi breakpoint - prije poziva metode
+            int x = 0;
+
+            // ovdje se poziva metoda koja se analizira - verzije sa tuningom ili bez
+            klasa.PretragaUposlenikaPoNazivu("Ime0");
+
+            // drugi breakpoint - nakon poziva metode
+            int y = 0;
+
+            Assert.IsTrue(true);
+        }
+    }
+}
