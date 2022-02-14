aee1df1bee3dfdcf69db28bf616e9b29a7f0f74b
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-02-14 10:30:20 +0100

    Commit vol 2 sa gitignore

---
+++ PrivatnaKlinika/PrivatnaKlinika/MedicinskoOsoblje.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivatnaKlinika
+{
+    public class MedicinskoOsoblje : Osoblje
+    {
+        int posjete = 0;
+        double plata=1000, bonus = 0.01;
+        Ordinacija ordinacija;
+        public int Posjete
+        {
+            get
+            {
+                return posjete;
+            }
+
+            set
+            {
+                posjete = value;
+            }
+        }
+
+        public double Plata
+        {
+            get
+            {
+                return plata;
+            }
+
+            set
+            {
+                plata = value;
+            }
+        }
+
+        public Ordinacija Ordinacija
+        {
+            get
+            {
+                return ordinacija;
+            }
+
+            set
+            {
+                ordinacija = value;
+            }
+        }
+
+        public MedicinskoOsoblje (string i, string p, string u, string pa, Ordinacija o) : base(i, p, u, pa) { Ordinacija = o; }
+
+        public MedicinskoOsoblje(string i, string p, string u, string pa) : base(i, p, u, pa) { }
+        public void obracunajPlatu ()
+        {
+            int posjete2 = 0;
+            if (Posjete > 20) posjete2 = 20;
+            else posjete2 = Posjete;
+            if (Password[Password.Length-1]<'0' || Password[Password.Length - 1] > '9') Plata += bonus * posjete2;
+        }
+    }
+}
---
+++ PrivatnaKlinika/UnitTestovi/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrivatnaKlinika;
+using System;
+
+namespace UnitTestovi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static Ordinacija o;
+        static Pacijent p;
+
+        /// <summary>
+        /// Inicijalizacija podataka koja se vrši samo jednom
+        /// </summary>
+        [ClassInitialize]
+        public static void PočetnaInicijalizacija(TestContext context)
+        {
+            o = new Ordinacija("Opća ordinacija");
+        }
+
+        /// <summary>
+        /// Inicijalizacija podataka koja se vrši prije svakog testa
+        /// </summary>
+        [TestInitialize]
+        public void InicijalizacijaPrijeSvakogTesta()
+        {
+            p = new Pacijent("Ime", "Prezime", DateTime.Parse("01/01/1996"), "0101996170001", "Muško", "Adresa", "Slobodan",
+                "Sistematski pregled", DateTime.Now, "ZD-001");
+        }
+
+        /// <summary>
+        /// Test dodavanja pacijenta u red ordinacije
+        /// </summary>
+        [TestMethod]
+        public void TestDodavanjePacijentaURed()
+        {
+            // izvršavanje funkcionalnosti
+            o.dodajPacijenta(p);
+
+            // provjera očekivanih rezultata 
+            Assert.IsTrue(o.Zauzetost > 0);
+            Assert.IsTrue(o.PacijentiURedu.Count == 1);
+        }
+
+        /// <summary>
+        /// Test obrade pacijenta kada je već u redu ordinacije
+        /// </summary>
+        [TestMethod]
+        public void TestObradePacijenta()
+        { 
+            // izvršavanje funkcionalnosti
+            Pacijent p2 = o.sljedeci();
+            
+            // provjera očekivanih rezultata
+            Assert.AreEqual(o.Zauzetost, 0);
+            Assert.AreEqual(o.PacijentiURedu.Count, 0);
+            Assert.IsTrue(p.Maticni == p2.Maticni);
+            Assert.AreNotSame(p, p2);
+        }
+
+        /// <summary>
+        /// Test pokušaja obrade pacijenta kada je ordinacija prazna
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestObradePacijentaIzuzetak()
+        {
+            Ordinacija praznaOrdinacija = new Ordinacija("Ambulanta");
+            praznaOrdinacija.sljedeci();
+        }
+    }
+}
