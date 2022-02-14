abf5359582d31dc3e1ed0b59d35c91a43194eead
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-02-14 10:32:39 +0100

    Commit vol 2 sa gitignore

---
+++ SistemZaGrijanje/SistemZaGrijanje/ITermostat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemZaGrijanje
+{
+    public interface ITermostat
+    {
+        public List<double> Temperature 
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public double dajTemperaturu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
